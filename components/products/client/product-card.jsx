3ce7ac654f5c44ae154ab30aadb18fb389f0dfd1
helpimblind <adrian.adamsgit@outlook.com>
2026-03-20 01:48:05 +0200

    Animation Fixes - V2

--- components/products/client/product-card.jsx
+++ components/products/client/product-card.jsx
@@ -50,6 +50,7 @@ export default function ProductCard({ data }) {
                             className={`${baseStyles} ${hover ? 'opacity-0' : 'opacity-100'}`}
                             fill
                             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                            loading='lazy'
                         />
                     )}
 
