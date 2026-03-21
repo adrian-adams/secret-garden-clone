3d59c2b095cdd6461ed2f2c4472d816895e9b660
helpimblind <adrian.adamsgit@outlook.com>
2026-03-22 01:07:11 +0200

    Added GA

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import "./globals.css";
+// Nextjs
+import Script from "next/script";
 // Fonts
 import { Geist, Geist_Mono } from "next/font/google";
 import { League_Spartan } from "next/font/google";
@@ -43,6 +45,23 @@ export default async function RootLayout({ children }) {
           {children}
         </main>
         <Footer />
+
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script 
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+                __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+                `
+            }}
+        />
       </body>
     </html>
   )
