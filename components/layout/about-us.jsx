32c8d0ace1242def05e1efde1cd4a4b27f3c6baa
helpimblind <adrian.adamsgit@outlook.com>
2026-03-21 03:38:25 +0200

    AF - V12

--- components/layout/about-us.jsx
+++ components/layout/about-us.jsx
@@ -44,14 +44,15 @@ export default function AboutUS() {
 
     return (
         <div ref={container}>
-            <Heading title="About Us" className="gsap-au-text" />
-            <p className="gsap-au-text text-md md:text-2xl">
-                We are a small plant store with three locations in NYC. Come shop at any of our locations or order plants from the comfort of your couch. labore et dolor magna aliqua. Ut enim ad minim velit, quis nostrud exercitation porttitor.
-            </p>
-            <br />
-            <p className="gsap-au-text text-md md:text-2xl">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris rhoncus aenean vel elit. Tristique nulla aliquet enim tortor at auctor urna mauris commodo.
-            </p>
+            <div className='gsap-au-text'>
+                <Heading title="About Us" />
+                <p className="sg-font-medium">
+                    We are a small plant store with three locations in NYC. Come shop at any of our locations or order plants from the comfort of your couch. labore et dolor magna aliqua. Ut enim ad minim velit, quis nostrud exercitation porttitor.
+                    <br />
+                    <br />
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris rhoncus aenean vel elit. Tristique nulla aliquet enim tortor at auctor urna mauris commodo.
+                </p>
+            </div>
         </div>
     )
 }
